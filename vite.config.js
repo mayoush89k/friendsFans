f2f98bbbfd9692124102e901a149076bcdd9d4a3
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-12-09 21:40:46 +0200

    netlift deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+//// BEGINNING OF MODIFICATION 
+///  add build property here ->
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+//// END OF MODIFICATION 
+});
